577102210f25cf784007e8995f427f2e12f3f531
antony <Antony Church>
2016-01-14 21:53:48 +0000

    Added basic notification stuff to RSSFeedCheckReciever

--- app/src/main/java/com/happycactus/blogminder/MainActivity.java
+++ app/src/main/java/com/happycactus/blogminder/MainActivity.java
@@ -1,7 +1,14 @@
 package com.happycactus.blogminder;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setAlarm(View v){
+        Long time = new GregorianCalendar().getTimeInMillis() + 60 * 1000;
+
+        Intent alarmIntent = new Intent(this, RSSFeedCheckReceiver.class);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time,
+                PendingIntent.getBroadcast(this, 1, alarmIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT));
+
+        Toast.makeText(this, "Alarm Scheduled for 1 min", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/happycactus/blogminder/RSSFeedCheckReceiver.java
+++ app/src/main/java/com/happycactus/blogminder/RSSFeedCheckReceiver.java
@@ -1,8 +1,13 @@
 package com.happycactus.blogminder;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by antony on 13/01/16.
@@ -10,6 +15,19 @@ import android.content.Intent;
 public class RSSFeedCheckReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Do alarm stuff.
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
     }
 }
