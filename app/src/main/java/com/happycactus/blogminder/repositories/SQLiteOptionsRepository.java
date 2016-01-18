3912ab6f7fcf52adfeee3a8cede338683e152e39
Antony Church <achurch44.ac@gmail.com>
2016-01-18 22:22:28 +0000

    Added SQLiteOptionsRepository

---
+++ app/src/main/java/com/happycactus/blogminder/repositories/SQLiteOptionsRepository.java
@@ -0,0 +1,46 @@
+package com.happycactus.blogminder.repositories;
+
+import android.content.Context;
+
+import com.happycactus.blogminder.database.OptionsDatabaseHelper;
+
+import org.joda.time.DateTime;
+
+public class SQLiteOptionsRepository implements IOptionsRepository {
+
+    OptionsDatabaseHelper mOptionsDatabaseHelper;
+
+    public  SQLiteOptionsRepository(Context context){
+        mOptionsDatabaseHelper = new OptionsDatabaseHelper(context);
+    }
+
+    @Override
+    public DateTime GetNextDeadline(DateTime From, int NumberOfDays) {
+        return null;
+    }
+
+    @Override
+    public void SetNextDeadLine(DateTime Deadline) {
+
+    }
+
+    @Override
+    public void SetPublishDateNodeName(String PublishDateNodeName) {
+
+    }
+
+    @Override
+    public String GetPublishDateNodeName() {
+        return null;
+    }
+
+    @Override
+    public String GetRSSFeedUrl() {
+        return null;
+    }
+
+    @Override
+    public void SetRSSFeedUrl(String RSSFeedUrl) {
+
+    }
+}
