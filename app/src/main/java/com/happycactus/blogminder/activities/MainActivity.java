346b7c6c5daea0a7b481feaa8c442691b6137e6c
Antony Church <achurch44.ac@gmail.com>
2016-01-27 22:25:52 +0000

    Started to bind all UI in with the Activity

--- app/src/main/java/com/happycactus/blogminder/activities/MainActivity.java
+++ app/src/main/java/com/happycactus/blogminder/activities/MainActivity.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.happycactus.blogminder.R;
 import com.happycactus.blogminder.logic.IApplicationLogic;
+import com.happycactus.blogminder.logic.IRSSLogic;
 import com.happycactus.blogminder.logic.LiveApplicationLogic;
+import com.happycactus.blogminder.logic.LiveRSSLogic;
 import com.happycactus.blogminder.receivers.RSSFeedCheckReceiver;
+import com.happycactus.blogminder.repositories.IOptionsRepository;
+import com.happycactus.blogminder.repositories.SQLiteOptionsRepository;
 
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
@@ -23,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
     private final int MillisecondsInADay = 86400000;
     private IApplicationLogic mApplicationLogic;
+    private IOptionsRepository mOptionsRepository;
+    private IRSSLogic mRSSLogic;
+
+    private Spinner mNewPostNeededSpinner;
+    private EditText mNewPostRangeTextBox;
+    private Spinner mCheckFrequencySpinner;
+    private Spinner mCheckTimeHourSpinner;
+    private Spinner mCheckTimeMinuteSpinner;
+    private EditText mFeedUrlTextBox;
+    private EditText mDateNodeTextBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mApplicationLogic = new LiveApplicationLogic();
+        mOptionsRepository = new SQLiteOptionsRepository(this);
+        mRSSLogic = new LiveRSSLogic();
+
+        mNewPostNeededSpinner = (Spinner) findViewById(R.id.newPostNeededSpinner);
+        mNewPostRangeTextBox = (EditText) findViewById(R.id.dayRangeEditText);
+        mCheckFrequencySpinner = (Spinner) findViewById(R.id.checkFrequencySpinner);
+        mCheckTimeHourSpinner = (Spinner) findViewById(R.id.checkHourSpinner);
+        mCheckTimeMinuteSpinner = (Spinner) findViewById(R.id.checkMinuteSpinner);
+        mFeedUrlTextBox = (EditText) findViewById(R.id.feedUrlEditText);
+        mDateNodeTextBox = (EditText) findViewById(R.id.dateNodeNameEditText);
+
+        fillInUI();
+
+        mApplicationLogic = new LiveApplicationLogic();
+    }
+
+    private void fillInUI(){
+        //Fill in mNewPostNeededSpinner
+        mNewPostRangeTextBox.setText(Integer.toString(mOptionsRepository.GetRange()));
+        //Fill in mCheckFrequencySpinner
+        //Fill in mCheckTimeHourSpinner
+        //Fill in mCheckTimeMinuteSpinner
+        mFeedUrlTextBox.setText(mOptionsRepository.GetRSSFeedUrl());
+        mDateNodeTextBox.setText(mOptionsRepository.GetPublishDateNodeName());
     }
 
     public void setAlarm(View v){
